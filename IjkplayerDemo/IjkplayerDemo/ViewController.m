4f1b55cb6cd6909c306c44dd6d159bac33915584
肖扬 <xy_dtb@163.com>
2018-06-13 17:32:21 +0800

    上传demo

--- IjkplayerDemo/IjkplayerDemo/ViewController.m
+++ IjkplayerDemo/IjkplayerDemo/ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "ViewController.h"
+#import <IJKMediaFramework/IJKMediaFramework.h>
+
+static NSString *const mp4Url = @"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+static NSString *const m3u8Url = @"http://live.hkstv.hk.lxdns.com/live/hks/playlist.m3u8";
 
 @interface ViewController ()
+
+@property (nonatomic, strong) NSURL *url;
+@property (nonatomic, strong) id <IJKMediaPlayback> player;
+@property (nonatomic, strong) UIView *PlayerView;
+@property (nonatomic, strong) UIButton *playButton;
 
 @end
 
@@ -17,8 +26,171 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setUI];
+    [self addObservers];
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear: animated];
+    if (![_player isPlaying]) {
+        [_player prepareToPlay];
+    }
+}
+
+- (void)setUI {
+    _url = [NSURL URLWithString:m3u8Url];
+    _player = [[IJKFFMoviePlayerController alloc] initWithContentURL:_url withOptions:nil];
+    [_player setScalingMode:(IJKMPMovieScalingModeAspectFit)];
+    
+    UIView *playerView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 180)];
+    playerView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:playerView];
+    
+    _PlayerView = playerView;
+    
+    UIView *playView = [self.player view];
+    playView.frame = _PlayerView.bounds;
+    playView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [_PlayerView addSubview:playView];
+    [_PlayerView insertSubview:playView atIndex:1];
+    
+    UIButton *playButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    playButton.frame = CGRectMake(50, 500, self.view.bounds.size.width - 100, 50);
+    [playButton setTitle:@"暂停" forState:(UIControlStateNormal)];
+    [playButton setBackgroundColor:[UIColor blackColor]];
+    [playButton setTintColor:[UIColor whiteColor]];
+    [playButton addTarget:self action:@selector(playMovie:) forControlEvents:(UIControlEventTouchUpInside)];
+    [self.view addSubview:playButton];
+}
+
+- (void)playMovie:(UIButton *)sender {
+    if (![self.player isPlaying]) {
+        [self.player play];
+        [sender setTitle:@"暂停" forState:(UIControlStateNormal)];
+    } else {
+        [self.player pause];
+        [sender setTitle:@"开始" forState:(UIControlStateNormal)];
+    }
+}
+
+
+- (void)addObservers {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(loadStateDidChange:)
+                                                 name:IJKMPMoviePlayerLoadStateDidChangeNotification
+                                               object:_player];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackFinish:)
+                                                 name:IJKMPMoviePlayerPlaybackDidFinishNotification
+                                               object:_player];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(mediaIsPreparedToPlayDidChange:)
+                                                 name:IJKMPMediaPlaybackIsPreparedToPlayDidChangeNotification
+                                               object:_player];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(moviePlayBackStateDidChange:)
+                                                 name:IJKMPMoviePlayerPlaybackStateDidChangeNotification
+                                               object:_player];
+}
+
+#pragma mark --- Observers
+
+/**
+ 播放状态监视
+
+ @param notification 观察者
+ */
+- (void)loadStateDidChange:(NSNotification*)notification {
+    IJKMPMovieLoadState loadState = _player.loadState;
+    
+    if ((loadState & IJKMPMovieLoadStatePlaythroughOK) != 0) {
+        NSLog(@"LoadStateDidChange: IJKMovieLoadStatePlayThroughOK: %d\n",(int)loadState);
+    }else if ((loadState & IJKMPMovieLoadStateStalled) != 0) {
+        NSLog(@"loadStateDidChange: IJKMPMovieLoadStateStalled: %d\n", (int)loadState);
+    } else {
+        NSLog(@"loadStateDidChange: ???: %d\n", (int)loadState);
+    }
+}
+
+
+/**
+ 视频播放结束的原因监视
+
+ @param notification 观察者
+ */
+- (void)moviePlayBackFinish:(NSNotification*)notification {
+    int reason =[[[notification userInfo] valueForKey:IJKMPMoviePlayerPlaybackDidFinishReasonUserInfoKey] intValue];
+    switch (reason) {
+        case IJKMPMovieFinishReasonPlaybackEnded:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackEnded: %d\n", reason);
+            break;
+            
+        case IJKMPMovieFinishReasonUserExited:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonUserExited: %d\n", reason);
+            break;
+            
+        case IJKMPMovieFinishReasonPlaybackError:
+            NSLog(@"playbackStateDidChange: IJKMPMovieFinishReasonPlaybackError: %d\n", reason);
+            break;
+            
+        default:
+            NSLog(@"playbackPlayBackDidFinish: ???: %d\n", reason);
+            break;
+    }
+}
+
+
+/**
+ 视频将要播放
+
+ @param notification 观察者
+ */
+- (void)mediaIsPreparedToPlayDidChange:(NSNotification*)notification {
+    NSLog(@"mediaIsPrepareToPlayDidChange\n");
+}
+
+
+/**
+ 视频状态变化监视
+
+ @param notification 观察者
+ */
+- (void)moviePlayBackStateDidChange:(NSNotification*)notification {
+    switch (_player.playbackState) {
+        case IJKMPMoviePlaybackStateStopped:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: stoped", (int)_player.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStatePlaying:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: playing", (int)_player.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStatePaused:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: paused", (int)_player.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStateInterrupted:
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: interrupted", (int)_player.playbackState);
+            break;
+            
+        case IJKMPMoviePlaybackStateSeekingForward:
+        case IJKMPMoviePlaybackStateSeekingBackward: {
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: seeking", (int)_player.playbackState);
+            break;
+        }
+            
+        default: {
+            NSLog(@"IJKMPMoviePlayBackStateDidChange %d: unknown", (int)_player.playbackState);
+            break;
+        }
+    }
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- README.md
+++ README.md
@@ -1,2 +1,134 @@
 # ijkplayerDemo
+
 # 基于ijkplayer做的直播demo
+
+### 一、下载ijkplayer
+
+在一个合适的位置新建一个文件夹, 假设为桌面, 文件夹名为 **ijkplayer**
+
+```
+# 进入到刚刚新建的文件夹内
+cd ~/Desktop/ijkplayer/
+
+# 获取ijkplayer源码
+git clone https://github.com/Bilibili/ijkplayer.git ijkplayer-ios
+
+# 进入源码目录
+cd ijkplayer-ios
+
+```
+
+> 目录结构：
+
+![image](https://upload-images.jianshu.io/upload_images/1803339-c3b8f8bcae855612.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/430)
+
+### 二、编译 ijkplayer
+
+#### 1.获取 ffmpeg 并初始化
+> ./init-ios.sh
+
+#### 2.添加 https 支持
+最后会生成支持 https 的静态文件 libcrypto.a 和 libssl.a, 如果不需要可以跳过这一步
+
+```
+# 获取 openssl 并初始化
+./init-ios-openssl.sh
+
+cd ios
+
+# 在模块文件中添加一行配置 以启用 openssl 组件
+echo 'export COMMON_FF_CFG_FLAGS="$COMMON_FF_CFG_FLAGS --enable-openssl"' >> ../config/module.sh
+
+```
+
+#### 3.编译
+```
+./compile-ffmpeg.sh clean
+
+# 编译openssl, 如果不需要https可以跳过这一步
+./compile-openssl.sh all
+
+# 编译ffmpeg
+./compile-ffmpeg.sh all
+```
+
+**如果提示错误:**
+
+```
+./libavutil/arm/asm.S:50:9: error: unknown directive
+        .arch armv7-a
+        ^
+make: *** [libavcodec/arm/aacpsdsp_neon.o] Error 1
+```
+
+> 报错原因是因为xcode对32位的支持弱化了，可以在compile-ffmpeg.sh里面删除**armv7**
+FF_ALL_ARCHS_IOS8_SDK="arm64 i386 x86_64"
+
+> 再次执行
+./compile-ffmpeg.sh all
+
+### 三、打包IJKMediaFramework.framework框架
+
+#### 1.首先打开工程IJKMediaPlayer.xcodeproj, 位置如下图:
+
+![image](https://upload-images.jianshu.io/upload_images/1803339-607cc84c212faf90.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/700)
+
+#### 2.添加 openssl 相关包以支持 https
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/2087622254.png)
+
+#### 3.打包 framwork
+> 1.配置 Release 模式如果下图
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/666544080.png)
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/4092292338.png)
+
+> 2. 配置模拟器 framework
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/3861753393.png)
+
+> 如图操作，然后 command+b 编译即可
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/3830063398.png)
+
+#### 4.合并 framework
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/234251722.png)
+
+> 准备合并:
+
+打开终端, 先 cd 到 Products 目录下
+
+然后执行: lipo -create 真机framework路径 模拟器framework路径 -output 合并的文件路径
+
+```
+lipo -create Release-iphoneos/IJKMediaFramework.framework/IJKMediaFramework Release-iphonesimulator/IJKMediaFramework.framework/IJKMediaFramework -output IJKMediaFramework
+
+```
+![image](https://blog.wskfz.com/usr/uploads/2018/04/1012295434.png)
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/3198101866.png)
+
+
+
+### 四、集成ijkplayer
+
+#### 1.导入 framework
+
+直接将 IJKMediaFramework.framework 拖入到工程中即可
+
+注意记得勾选 Copy items if needed 和 对应的 target
+
+#### 2.添加依赖库
+
+![image](https://blog.wskfz.com/usr/uploads/2018/04/1293654705.png)
+
+### 五、demo
+添加ijkplayer头文件
+```
+#import <IJKMediaFramework/IJKMediaFramework.h>
+```
+编译无误，表示集成成功~
+
+

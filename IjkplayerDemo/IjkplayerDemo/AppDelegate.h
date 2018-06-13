c9c63902649b4d185ab0d3af6bf8d5cada786f5c
肖扬 <xy_dtb@163.com>
2018-06-13 13:57:53 +0800

    ijkplayerDemo

---
+++ IjkplayerDemo/IjkplayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IjkplayerDemo
+//
+//  Created by HXB-xiaoYang on 2018/6/13.
+//  Copyright © 2018年 VansXY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
